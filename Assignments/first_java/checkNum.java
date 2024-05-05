8d7476227b3957367772e63e5a5429528036917a
Ashish Kanan <ashishkananconnect@gmail.com>
2024-05-05 15:25:05 +0530

    Update checkNum.java
    The comment is transferred to the first line of code to make it look more simplistic and readable.

--- Assignments/first_java/checkNum.java
+++ Assignments/first_java/checkNum.java
@@ -1,7 +1,7 @@
+//Write a program to print whether a number is even or odd, also take input from the user.
 import java.util.Scanner;
 public class checkNum {
     public static void main(String[] args) {
-        //Write a program to print whether a number is even or odd, also take input from the user.
         Scanner input = new Scanner(System.in);
         int num = input.nextInt();
         if (num % 2 == 0){
