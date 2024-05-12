d9f1cc3971e3d2065bcceeb9f6f3f4dfc5e24eab
Ashish Kanan <ashishkananconnect@gmail.com>
2024-05-12 14:37:59 +0530

    Delete Assignments directory

--- Assignments/first_java/greetMessage.java
+++
@@ -1,9 +0,0 @@
-//Take name as input and print a greeting message for that particular name.
-import java.util.Scanner;
-public class greetMessage {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String name = input.next();
-        System.out.println("Hello " + name + ".Nice to meet you!" );
-    }
-}
