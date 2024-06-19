00ad54643dd636188c60fbfd622ddb128eba99be
Ashish Kanan <ashishkananconnect@gmail.com>
2024-06-19 12:17:37 +0530

    Update Solution

--- Snakes and Ladders: The Quickest Way Up/Solution.cpp
+++ Snakes and Ladders: The Quickest Way Up/Solution.cpp
@@ -1,1 +1,167 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'quickestWayUp' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. 2D_INTEGER_ARRAY ladders
+ *  2. 2D_INTEGER_ARRAY snakes
+ */
+
+int quickestWayUp(vector<vector<int>> ladders, vector<vector<int>> snakes) {
+    // Initialize variables
+    int s = 101;
+    vector<int> board(s, 0);
+    vector<int> status(s, -1);
+    queue<int> Q;
+    Q.push(1);
+    status[1] = 0;
+    
+    // Set ladders on the board
+    for (const auto& ladder : ladders) {
+        int a = ladder[0];
+        int b = ladder[1];
+        board[a] = b;
+    }
+    
+    // Set snakes on the board
+    for (const auto& snake : snakes) {
+        int a = snake[0];
+        int b = snake[1];
+        board[a] = b;
+    }
+    
+    // Main program begins now
+    while (!Q.empty()) {
+        int x = Q.front();
+        Q.pop();
+        
+        for (int i = 1; i <= 6; i++) { // Dice throw
+            int nextPos = x + i;
+            if (nextPos > 100) break; // Skip positions beyond 100
+            
+            int finalPos = (board[nextPos] != 0) ? board[nextPos] : nextPos;
+            
+            if (status[finalPos] == -1) { // Unvisited
+                status[finalPos] = status[x] + 1; // Update status
+                Q.push(finalPos);
+                
+                if (finalPos == 100) { // Reached position 100
+                    return status[100];
+                }
+            }
+        }
+    }
+    
+    return -1; // If reaching position 100 is not possible
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        vector<vector<int>> ladders(n);
+
+        for (int i = 0; i < n; i++) {
+            ladders[i].resize(2);
+
+            string ladders_row_temp_temp;
+            getline(cin, ladders_row_temp_temp);
+
+            vector<string> ladders_row_temp = split(rtrim(ladders_row_temp_temp));
+
+            for (int j = 0; j < 2; j++) {
+                int ladders_row_item = stoi(ladders_row_temp[j]);
+
+                ladders[i][j] = ladders_row_item;
+            }
+        }
+
+        string m_temp;
+        getline(cin, m_temp);
+
+        int m = stoi(ltrim(rtrim(m_temp)));
+
+        vector<vector<int>> snakes(m);
+
+        for (int i = 0; i < m; i++) {
+            snakes[i].resize(2);
+
+            string snakes_row_temp_temp;
+            getline(cin, snakes_row_temp_temp);
+
+            vector<string> snakes_row_temp = split(rtrim(snakes_row_temp_temp));
+
+            for (int j = 0; j < 2; j++) {
+                int snakes_row_item = stoi(snakes_row_temp[j]);
+
+                snakes[i][j] = snakes_row_item;
+            }
+        }
+
+        int result = quickestWayUp(ladders, snakes);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
